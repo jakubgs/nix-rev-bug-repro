3a3dfad415f651b38735ced062e2df58c35246a0
markoburcul <marko@status.im>
2026-05-29 12:55:40 +0200

    flake: update flake utils rev but not the flake lock
    Signed-off-by: markoburcul <marko@status.im>

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "nix-rev-bug-repro";
 
   inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-25.11;
-  inputs.flake-utils.url = "github:numtide/flake-utils?rev=c1dfcf08411b08f6b8615f7d8971a2bfa81d5e8a";
+  inputs.flake-utils.url = "github:numtide/flake-utils?rev=11707dc2f618dd54ca8739b309ec4fc024de578b";
 
   outputs = { self, nixpkgs, flake-utils }: let
     pkgs = import nixpkgs { system = "x86_64-linux"; };
