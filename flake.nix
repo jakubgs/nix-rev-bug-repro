d2f07037edeaefe6361fa286c6ae595eddfb2841
markoburcul <marko@status.im>
2026-05-29 12:55:13 +0200

    flake: add new input
    Signed-off-by: markoburcul <marko@status.im>

--- flake.nix
+++ flake.nix
@@ -2,8 +2,9 @@
   description = "nix-rev-bug-repro";
 
   inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-25.11;
+  inputs.flake-utils.url = "github:numtide/flake-utils?rev=c1dfcf08411b08f6b8615f7d8971a2bfa81d5e8a";
 
-  outputs = { self, nixpkgs }: let
+  outputs = { self, nixpkgs, flake-utils }: let
     pkgs = import nixpkgs { system = "x86_64-linux"; };
   in {
     packages."x86_64-linux".default = pkgs.stdenv.mkDerivation {
