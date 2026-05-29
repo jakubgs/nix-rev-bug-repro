78d76218abc7638bedd4d08c443be1104790eb50
markoburcul <marko@status.im>
2026-05-29 12:18:05 +0200

    flake: add dummy cache
    Signed-off-by: markoburcul <marko@status.im>

--- flake.nix
+++ flake.nix
@@ -1,15 +1,15 @@
 {
   description = "nix-rev-bug-repro";
 
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-25.11;
+  nixConfig.extra-trusted-public-keys = [ "dummy-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" ];
 
-  outputs = { self, nixpkgs }: let
-    pkgs = import nixpkgs { system = "x86_64-linux"; };
-  in {
-    packages."x86_64-linux".default = pkgs.stdenv.mkDerivation {
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+
+  outputs = { self, nixpkgs }: {
+    packages.x86_64-linux.default = (import nixpkgs { system = "x86_64-linux"; }).stdenv.mkDerivation {
       name = "nix-rev-bug-repro";
       src = self;
-      commit = self.sourceInfo.rev or "unknown";
+      commit = self.rev or "MISSING";
       builder = ./builder.sh;
       meta.mainProgram = "main";
     };
